cbce17dd18aa630718fc3f52169e9bfcb6110078
P N <pnzriyas@amazon.com>
2025-08-22 14:58:50 +0530

    Deploy responsive UI, menu CRUD, orders board

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+reactStrictMode: true,
+swcMinify: true,
+productionBrowserSourceMaps: false,
+eslint: { ignoreDuringBuilds: true },
+typescript: { ignoreBuildErrors: true },
+images: { unoptimized: true }
+}
