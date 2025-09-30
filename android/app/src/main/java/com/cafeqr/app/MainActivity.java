292bfaf3b236fedc30c89a2697cffea4097fc763
Riyas <pnriyas50@gmail.com>
2025-10-01 04:37:37 +0530

    latest updates

--- android/app/src/main/java/com/cafeqr/app/MainActivity.java
+++
@@ -1,8 +0,0 @@
-// Final, simplified MainActivity.java
-package com.cafeqr.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    // There is no onCreate method here.
-}
